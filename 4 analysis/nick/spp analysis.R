fe0494d7f19c13cb492ab9d64737f850da4c1c05
npm27 <35810320+npm27@users.noreply.github.com>
2018-02-21 23:05:20 -0600

    starting spp analysis

---
+++ 4 analysis/nick/spp analysis.R
@@ -0,0 +1,35 @@
+##setup
+dat = read.csv("combined_spp_file.csv")
+
+options(sipen = 999)
+
+####models####
+##t_ldt_RT
+##first step (word info)
+model1 = lm(t_ldt_RT ~ PLength + PSubFreq + POrthoN + TLength + TSubFreq + TOrthoN, data = spp_noout) 
+summary(dat)
+
+##second step (association)
+model2 = lm(t_ldt_RT ~ PLength + PSubFreq + POrthoN + TLength + TSubFreq + TOrthoN +
+              FAS + BAS + PCueFanOut + TTargetFanIn, data = dat) 
+summary(model2)
+lm.beta(model2)
+
+##third step (semantics)
+model3 = lm(t_ldt_RT ~ PLength + PSubFreq + POrthoN + TLength + TSubFreq + TOrthoN +
+              FAS + BAS + PCueFanOut + TTargetFanIn +
+              jcn + root + raw + affix, data = dat) ##need to add in nelson stuff
+summary(model3)
+
+##fourth step (thematics)
+model4 = lm(t_ldt_RT ~ PLength + PSubFreq + POrthoN + TLength + TSubFreq + TOrthoN +
+              FAS + BAS + PCueFanOut + TTargetFanIn +
+              jcn + root + raw + affix +
+              LSA + BEAGLEcosine, data = dat) ##need to add in cosine/mandera stuff
+summary(model4) 
+
+##t_LDT_Z
+
+##t_name_RT
+
+##t_name_Z
